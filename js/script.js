d1400ffa92bc1f3cfbfddf013214f5331df206c3
Martyna Michalak-Halinska <martyna.michalak@gmail.com>
2021-05-16 19:08:28 +0200

    add new task

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li> 
+                <li class="list__item"> 
                     ${task.content}
                 </li>
             `;
@@ -23,10 +23,30 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        };
+        addNewTask(newTaskContent);
+        
+
+        render();
+    };
 
     const init = () => {
         render();
 
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
     };
 
 
