54a79651f71d77b7cbb0d85b8771028db57bf29b
Martyna Michalak-Halinska <martyna.michalak@gmail.com>
2021-05-17 08:25:12 +0200

    added focus

--- js/script.js
+++ js/script.js
@@ -15,18 +15,29 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li class="list__item"> 
+                <li class="list__item js-item"> 
                     ${task.content}
                 </li>
             `;
         };
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        for (const task of tasks) {
+            if (task.done) {
+               document.querySelector(".js-item").classList.add("list__item--done");
+            }
+        }
     };
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
+    };
+
+    const clearFormAndFocus = () => {
+        document.querySelector(".js-newTask").value = "";
+        document.querySelector(".js-newTask").focus();
     };
 
     const onFormSubmit = (event) => {
@@ -37,7 +48,8 @@
             return;
         };
         addNewTask(newTaskContent);
-        
+
+        clearFormAndFocus();
 
         render();
     };
